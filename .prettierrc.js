10863f1d76ccaefa2987b76b9c05b47f9d3e09c3
Edward Kim <edward@cyberbird.co>
2021-03-19 21:40:00 -0500

    build: initializing build configuration

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	arrowParens: 'avoid',
+	overrides: [
+		{
+			files: '.*rc',
+			options: { parser: 'json' }
+		}
+	],
+	singleQuote: true,
+	trailingComma: 'none',
+	useTabs: true
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# classnames
+
+Reduces mixed-type arguments into a single HTML class attribute (really just a poor replica of [classnames](https://www.npmjs.com/package/classnames) made for fun).
