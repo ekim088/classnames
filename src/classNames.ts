a576f7984ea262606f7bac12c7489a69e85505f6
Edward Kim <edward@cyberbird.co>
2021-03-20 00:19:16 -0500

    feat: adding base and dedupe className functions

---
+++ src/classNames.ts
@@ -0,0 +1,47 @@
+export type ClassDictionary = Record<string, unknown>;
+
+export type ClassListArray = (string | number | true)[];
+
+export type ClassValue =
+	| string
+	| number
+	| boolean
+	| undefined
+	| null
+	| ClassDictionary
+	| ClassValue[];
+
+/**
+ * Reduces a list of arguments into a single class attribute value.
+ *
+ * @param classNameArgs A list of arguments to reduce.
+ * @returns A class attribute value.
+ */
+export default function classNames(...classNameArgs: ClassValue[]): string {
+	const classList: ClassListArray = [];
+
+	/**
+	 * Parses and pushes an argument to the class list.
+	 *
+	 * @param arg The argument to parse.
+	 */
+	const parseArg = (arg: ClassValue): void => {
+		if (!arg) return;
+
+		if (Array.isArray(arg)) {
+			arg.forEach(parseArg);
+		} else if (typeof arg === 'object') {
+			Object.keys(arg).forEach(
+				key => arg[key] && classList.push(key.trim())
+			);
+		} else if (typeof arg === 'string') {
+			const trimmed = arg.trim();
+			if (trimmed) classList.push(trimmed);
+		} else {
+			classList.push(arg);
+		}
+	};
+
+	classNameArgs.forEach(parseArg);
+	return classList.join(' ');
+}
---
+++ src/dedupe/index.ts
@@ -0,0 +1,42 @@
+import { ClassListArray, ClassValue } from '../classNames';
+
+/**
+ * Reduces a list of arguments into a single class attribute value. Filters out
+ * duplicate class names.
+ *
+ * @param classNameArgs A list of arguments to reduce.
+ * @returns A class attribute value without duplicates.
+ */
+export default function classNamesDedupe(
+	...classNameArgs: ClassValue[]
+): string {
+	const classList: ClassListArray = [];
+	let dictionary: Map<string, unknown> = new Map();
+
+	/**
+	 * Parses and pushes an argument to the class dictionary.
+	 *
+	 * @param arg The argument to parse.
+	 */
+	const parseArg = (arg: ClassValue): void => {
+		if (!arg) return;
+
+		if (Array.isArray(arg)) {
+			arg.forEach(parseArg);
+		} else if (typeof arg === 'object') {
+			dictionary = new Map([
+				...dictionary,
+				...new Map(Object.entries(arg))
+			]);
+		} else {
+			dictionary.set(`${arg}`, true);
+		}
+	};
+
+	classNameArgs.forEach(parseArg);
+	dictionary.forEach((value, key) => {
+		const trimmedKey = key.trim();
+		if (value && trimmedKey) classList.push(trimmedKey);
+	});
+	return classList.join(' ');
+}
